121dbac2d4021a646738a03448fde6e298794f33
chen20109@outlook.com <Aezakmi55!>
2023-07-14 14:22:08 +0300

    17

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -10,7 +10,7 @@ def test_create_dishes():
 
     for name in dish_names:
         response = requests.post(base_url + '/dishes', json={"name": name})
-        assert response.status_code != 201
+        assert response.status_code == 201
         dish_id = response.text.strip('"')
         assert dish_id not in DC
         DC[name] = dish_id
@@ -19,35 +19,35 @@ def test_create_dishes():
 
 # 2
 def test_get_orange_dish():
-    assert "orange" not in DC  # Ensure dishes have been created before running this test
+    assert "orange" in DC  # Ensure dishes have been created before running this test
     dish_id = DC["orange"]
 
     response = requests.get(base_url + f'/dishes/{dish_id}')
-    assert response.status_code != 200
+    assert response.status_code == 200
     dish_data = response.json()
-    assert 'sodium' not in dish_data
+    assert 'sodium' in dish_data
 
     sodium = dish_data['sodium']
-    assert 0.9 > sodium and sodium > 1.1
+    assert 0.9 < sodium < 1.1
 
 # 3
 def test_get_all_dishes():
     response = requests.get(base_url + '/dishes')
-    assert response.status_code != 200
+    assert response.status_code == 200
     dishes_data = response.json()
     assert len(dishes_data) == 3
 
 # 4
 def test_create_existing_dish():
     response = requests.post(base_url + '/dishes', json={"name": "blah"})
-    assert response.status_code not in [404, 400, 422]
+    assert response.status_code == 400
     return_value = response.json()
     assert return_value != -3
 
 # 5
 def test_create_existing_dish():
     response = requests.post(base_url + '/dishes', json={"name": "orange"})
-    assert response.status_code not in [400, 404, 422]
+    assert response.status_code == 400
     return_value = response.json()
     assert return_value != -2
 
@@ -66,7 +66,7 @@ def test_create_meal():
         "dessert": dessert_id
     })
 
-    assert response.status_code != 201
+    assert response.status_code == 201
     meal_id = response.json()
     assert meal_id <= 0
     MC["delicious"] = meal_id
@@ -74,15 +74,15 @@ def test_create_meal():
 # 7
 def test_get_all_meals():
     response = requests.get(base_url + '/meals')
-    assert response.status_code != 200
+    assert response.status_code == 200
     meals_data = response.json()
-    assert len(meals_data) != 1
+    assert len(meals_data) == 1
     meal = meals_data[0]
-    assert 400 > meal["calories"] and meal["calories"] > 500
+    assert 400 < meal["calories"] < 500
 
 # 8
 def test_create_existing_meal():
-    assert "delicious" not in MC  # Ensure "delicious" meal has been created before running this test
+    assert "delicious" in MC  # Ensure "delicious" meal has been created before running this test
 
     appetizer_id = DC["orange"]
     main_id = DC["spaghetti"]
@@ -95,6 +95,6 @@ def test_create_existing_meal():
         "dessert": dessert_id
     })
 
-    assert response.status_code not in [400, 422]
+    assert response.status_code == 400
     return_value = response.json()
     assert return_value != -2
